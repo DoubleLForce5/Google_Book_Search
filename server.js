3e0cd28a91673ad84caf0bb86fcfcb315d009707
Liv <lopez.olivia17@outlook.com>
2021-07-06 17:27:48 -0500

    created server.js, installed required packages, generated books model

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const routes = require('./routes');
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI ||
+"mongodb://localhost/googlebooks");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
